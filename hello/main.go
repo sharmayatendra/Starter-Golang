40f01e95624fee8825365afea4e3255f32723daa
sharmayatendra <yatendra948@gmail.com>
2023-10-16 01:40:11 +0530

    feat: Understanding variable declaration
    - How to declare variable in go?
    - Nuances of go lang like we can return multiple values
    - Shorthand syntax for variable declaration

--- hello/main.go
+++ hello/main.go
@@ -3,9 +3,29 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("Hello world")
 
 	// defining the variables
 
-	firstName := "Yaten"
-	fmt.Println("My firstName is", firstName)
+	var whatToSay string
+	var i int
+
+
+	whatToSay = "Good bye!!, cruel world"
+
+	fmt.Println(whatToSay)
+
+	i = 100
+	fmt.Println("i is set to", i)
+
+	// shorthand syntax of variable declration
+	whatWasSaid, otherThingSaid := saySomething()
+
+	fmt.Println("saySomething function has returned us", whatWasSaid, otherThingSaid)
+}
+
+// syntax of function i.e this function will going to return a `string`
+func saySomething() (string, string) {
+	// speciality of goLang, we can return multiple things from a single func.
+	return "something", "else"
 }
