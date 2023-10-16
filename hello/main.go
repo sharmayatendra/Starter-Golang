35b99be65098ba414b5763fcf258a8ff71aa7916
sharmayatendra <yatendra948@gmail.com>
2023-10-17 00:59:53 +0530

    feat: Syntax of pointers
    - Declaration and access of pointer

--- hello/main.go
+++ hello/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	fmt.Println("Hello world")
@@ -22,10 +25,25 @@ func main() {
 	whatWasSaid, otherThingSaid := saySomething()
 
 	fmt.Println("saySomething function has returned us", whatWasSaid, otherThingSaid)
+
+	var myStr string
+	myStr = "Black"
+
+	log.Println("myStr is set to", myStr)
+	changeUsingPointer(&myStr)
+
+	log.Println("After the func call myStr is set to", myStr)
+
 }
 
 // syntax of function i.e this function will going to return a `string`
 func saySomething() (string, string) {
 	// speciality of goLang, we can return multiple things from a single func.
 	return "something", "else"
+}
+
+// case of pointer and using it: use `&` for reference and `*` for value
+func changeUsingPointer(s *string) {
+	newValue := "White"
+	*s = newValue
 }
