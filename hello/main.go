96eec6c284d14dc7657fba8cb4f2dd705343f501
sharmayatendra <yatendra948@gmail.com>
2023-10-27 00:47:44 +0530

    feat: :label: Understanding types
    - Understanding `types` and `struct`
    - Scope of variable and function

--- hello/main.go
+++ hello/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+)
+
+// to get rid of making different variable create a `type`
+type User struct {
+	FirstName string
+	LastName string
+	PhoneNumber string
+	Age int
+	BirthDate time.Time
+}
+
+// defining variable like this only available it only in this package
+var spcl string
+
+// but if it starts with capital letter it will be available outside the package
+// well. Same is true for functions as well
+var Spcl string
 
 func main() {
-	fmt.Println("Hello from the Golang!!")
+	// take a variable and assign the values in it
+	user := User {
+		FirstName: "John",
+		LastName: "Doe",
+	}
+
+	log.Println(user.FirstName, user.LastName, user.Age, user.PhoneNumber, user.BirthDate)
 }
