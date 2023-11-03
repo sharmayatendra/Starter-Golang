5c6d9d0273075dedd0452f19232a91e9713b775b
sharmayatendra <yatendra948@gmail.com>
2023-11-04 01:32:30 +0530

    feat: Add `slice` declaration
    - How to declare slice?
    - Append data to slice
    - Different approaches to declare slice
    - Access few range of values instead of full slice

--- hello/main.go
+++ hello/main.go
@@ -27,4 +27,22 @@ func main() {
 	myMap2["mySelf"] = mySelf
 
 	log.Println(myMap["firstName"], myMap2["mySelf"].FirstName)
+
+
+	/// Slices:
+
+	var mySlice []string
+
+	
+	mySlice = append(mySlice, "yaten")
+	mySlice = append(mySlice, "SHarma")
+	mySlice = append(mySlice, "learning!!Go")
+
+	// another approach to declare values in slice
+	numbers := []int{1,2,3,4,5,6}
+	log.Println((mySlice))
+	log.Println(numbers)
+
+	// to print some range of numbers
+	log.Println(numbers[0:4])
 }
