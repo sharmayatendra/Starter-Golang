6d9c16d0385b1eab59480e21fd67664a99d362e5
sharmayatendra <yatendra948@gmail.com>
2023-11-06 01:20:52 +0530

    feat: Add rules about `if/else`
    - Implement how `decision-structure` are made here.

--- hello/main.go
+++ hello/main.go
@@ -1,65 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 )
 
 var s = "seven"
 
 func main() {
-	fmt.Println("Hello world")
+	// concept & syntax of if/else, switch statement:
 
-	// defining the variables
+	myNum := 100
+	isTrue := true
 
-	var whatToSay string
-	var i int
+	// test with two variable conditions
 
-	var s2 = "six"
+	if myNum > 100 && isTrue {
+		log.Println("myNum is greater than 100")
+	} else if myNum == 100 && !isTrue {
+		log.Println("myNum is equals to 100")
+	} else {
+		log.Println("myNum is lesser than 100")
+	}
 
 
-	whatToSay = "Good bye!!, cruel world"
 
-	fmt.Println(whatToSay)
 
-	i = 100
-	fmt.Println("i is set to", i)
 
-	log.Println("s is ", s)
-	log.Println("s2 is ", s2)
-
-	// shorthand syntax of variable declration
-	whatWasSaid, otherThingSaid := saySomething()
-
-	fmt.Println("saySomething function has returned us", whatWasSaid, otherThingSaid)
-
-	var myStr string
-	myStr = "Black"
-
-	log.Println("myStr is set to", myStr)
-	changeUsingPointer(&myStr)
-
-	log.Println("After the func call myStr is set to", myStr)
-
-	saySomethingAgain("calling saySomethingAgain")
-
-}
-
-// syntax of function i.e this function will going to return a `string`
-func saySomething() (string, string) {
-	// speciality of goLang, we can return multiple things from a single func.
-	return "something", "else"
-}
-
-// case of pointer and using it: use `&` for reference and `*` for value
-func changeUsingPointer(s *string) {
-	newValue := "White"
-	*s = newValue
-}
-
-// if i pass same param as `global variable` so when i will call this function it
-// will refer to that variable.
-func saySomethingAgain(s3 string) (string, string) {
-	log.Println("s from saySomethingAgain is ", s)
-	return s, "returned";
 }
